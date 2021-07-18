03cae13ecb0371c83df0518a26f80949948963d7
andres7mo <fernandosito2010@gmail.com>
2021-07-17 21:30:13 -0500

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfoComponent } from './info/info.component';
 import { PersonalComponent } from './personal/personal.component';
-import { UsuarioComponent } from './usuario/usuario.component';
-import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 const routes: Routes = [
 {path:'HOME' , component: InfoComponent},
 {path:'PERSONAL' , component: PersonalComponent},
-{path:'USUARIO' , component: UsuarioComponent},
+{path:'LOGIN' , component: LoginComponent},
 ];
 
 @NgModule({
